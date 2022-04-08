19bd4d968ab0e48f2c5b59768133ded84f1686c6
Diego Amatte <amattediegoomar@gmail.com>
2022-04-08 11:15:50 -0300

    Add jwt authentication

--- AuthPractice/Migrations/20220408132236_Initial-Migration.Designer.cs
+++ AuthPractice/Migrations/20220408132236_Initial-Migration.Designer.cs
@@ -4,6 +4,7 @@ using AuthPractice.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AuthPractice.Migrations
 {
     [DbContext(typeof(AuthPracticeContext))]
-    partial class AuthPracticeContextModelSnapshot : ModelSnapshot
+    [Migration("20220408132236_Initial-Migration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
